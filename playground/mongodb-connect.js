cdbbe72dabefa1df7394e8c00f06d983b078b630
musikele <michele.nasti@gmail.com>
2017-04-09 22:46:52 +0200

    first commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,34 @@
+const MongoClient = require('mongodb').MongoClient;
+
+// first argument: string - url
+// second argument: callbck
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.error('unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Todos').insert({
+    text: 'Something to do',
+    completed: false
+  }, (err, result) => {
+    if (err) {
+      return console.error('Unable to insert todo', err);
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  //Insert new doc into Users [name, age, location]
+  db.collection('Users').insert({
+    name: 'Michele',
+    age: 32,
+    location: 'Salerno, Italy'
+  }, (err, result) => {
+    if (err) {
+      return console.error('Unable to insert user', err);
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  db.close();
+});
